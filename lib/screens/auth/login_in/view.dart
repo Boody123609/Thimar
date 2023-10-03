2eea8b63ab8d2ad00be97068c9d7005ac507c5e3
Abd Elhady Alaa <76068570+Boody123609@users.noreply.github.com>
2023-10-03 09:48:45 +0300

    .

--- lib/screens/auth/login_in/view.dart
+++ lib/screens/auth/login_in/view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../../core/design/castama/btn.dart';
 import '../../../core/design/castama/input.dart';
 
